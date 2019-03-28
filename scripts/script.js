e7a8894543dfc2f9115933789bd1252ab3df3d01
Shayna <shaynawark@gmail.com>
2019-03-28 11:45:13 -0400

    responsive
    working on being responsive. basic phone size is looking quite good!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
-document.addEventListener('click', function(){
-    document.getElementsByClassName("work").classList = "clicked";
-});
+function click(elem) {
+    /*var work = document.getElementsByClassName('work');
+    var i;
+    for (i = 0; i > work.length; i++) {
+        work.classList.add = 'clicked';
+    }*/
+    elem.classList.add = 'clicked';
+}
