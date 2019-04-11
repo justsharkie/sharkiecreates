d054ccab1418545e5a5850eb31663d4a1ea30d4c
Shayna <shaynawark@gmail.com>
2019-04-11 10:49:09 -0400

    Desc. Layout
    The individual descriptions have all been laid out and are pretty. Yay! 
    
    Now they just need actual content.

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ function hide(elem1, elem2, elem3, elem4, elem5, desc1) {
     
     desc1.style.display = 'block';
 }
+
+function show(desc1, elem1, elem2, elem3, elem4, elem5) {
+    desc1.style.display = 'none';
+    
+    elem1.style.display = 'inline-block';
+    elem2.style.display = 'inline-block';
+    elem3.style.display = 'inline-block';
+    elem4.style.display = 'inline-block';
+    elem5.style.display = 'inline-block';
+}
