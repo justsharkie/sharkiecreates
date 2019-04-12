259129c06eb509ac2105bec3d3c8ec3ae90d5d44
Shayna <shaynawark@gmail.com>
2019-04-12 13:12:50 -0400

    Copy
    The official copy has been written!

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
-function hide(elem1, elem2, elem3, elem4, elem5, desc1) {
+function hide(elem1, elem2, elem3, elem4, elem5, elem6, desc1) {
     elem1.style.display = 'none';
     elem2.style.display = 'none';
     elem3.style.display = 'none';
     elem4.style.display = 'none';
     elem5.style.display = 'none';
+    elem6.style.display = 'none';
     
     desc1.style.display = 'block';
 }
 
-function show(desc1, elem1, elem2, elem3, elem4, elem5) {
+function show(desc1, elem1, elem2, elem3, elem4, elem5, elem6) {
     desc1.style.display = 'none';
     
     elem1.style.display = 'inline-block';
@@ -16,4 +17,5 @@ function show(desc1, elem1, elem2, elem3, elem4, elem5) {
     elem3.style.display = 'inline-block';
     elem4.style.display = 'inline-block';
     elem5.style.display = 'inline-block';
+    elem6.style.display = 'inline-block';
 }
